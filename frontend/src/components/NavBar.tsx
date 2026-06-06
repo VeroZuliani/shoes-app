198c1b829404a0bb5060b3936eab79d70cc7fbc4
VeroZuliani <vero.zuliani92@gmail.com>
2026-06-06 18:29:40 -0300

    feat: configurar menu collapse de navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,31 +1,87 @@
+import { useState } from "react"
 import Link from "./Link"
+
+interface LinkData{
+    text: string;
+    href: string;
+
+}
+
+const link : LinkData[] = [
+    {
+        href:"/",
+        text:"Home"    
+    },
+    {
+        href:"/about",
+        text:"About us"
+    },
+    {
+        href:"/products",
+        text:"Products"
+    },
+    {
+        href:"/testimonials",
+        text:"Testimonials"
+    },
+    {
+        href:"/contact",
+        text:"Contact us"
+    },
+    {
+        href:"/faq",
+        text:"FAQ"
+    },
+]
 
 
 const NavBar = () => {
 
+    // Estado para menú collapse
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toogleNavBar = () => {
+        setIsOpen(!isOpen)
+    }
+
   return (
-    <header className="w-full">
-        <div className="flex w-full px-4 lg:px-30">
-            <a href="/" className="z-50">
+    <header className="w-full items-center">
+        <div className="flex w-full px-4 lg:px-30 justify-between relative h-20">
+            <a href="/" className="z-50 flex items-center">
                 <img src="https://placehold.co/150x60?text=SPIKE" alt="" />
             </a>
 
-            <nav className="flex items-center mx-auto gap-16">
-                <ul className="flex gap-6">
-                    <Link href="/" text="Home"/>
-                    <Link href="/about" text="About us"/>
-                    <Link href="/products" text="Products"/>
-                    <Link href="/testimonials" text="Testimonials"/>
-                    <Link href="/contact" text="Contact us"/>
-                    <Link href="/faq" text="FAQ"/>
+            {/* isOpen es true: se muestra (flex), si es false: se esconde (hidden) */}
+            <nav className={`flex lg:items-center mx-auto lg:gap-16 flex-col lg:flex-row lg:static absolute top-5 right-15 lg:shadow-none shadow p-10 bg-white border border-gray-light rounded-md lg:border-none
+                ${isOpen ? "flex" : "hidden"} lg:flex `}
+            >
+                <ul className="flex gap-6 flex-col lg:flex-row">
+                    {link.map(( {href, text}: LinkData)=>(
+                        <Link 
+                            key={text}
+                            href={href}
+                            text={text}
+                        />
+                    ))}
                 </ul>
 
                 <div className="flex">
-                    <a href="/login" className="mx-4">
+                    <a href="/login" className="lg:mx-4 mt-5 lg:m-0" onClick={toogleNavBar}>
                         <img src="https://placehold.co/32?text=LOGIN" alt="" />
                     </a>
-                </div>
+                </div>  
             </nav>
+
+            {/* Boton Hamburguesa visible en Mobile */}
+            {/* Si está abierto muestra una cruz, si está cerrado muestra la hamburguesa tradicional */}
+            <button className="lg:hidden flex items-start mt-5" onClick={toogleNavBar}>
+                {isOpen ? 
+                    <img src="https://placehold.co/32?text=x" alt="" /> 
+                    : 
+                    <img src="https://placehold.co/32?text='☰'" alt="" /> 
+                }
+            </button>
+
         </div>
     </header>
   )
